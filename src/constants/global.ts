f8eaf4b9ef4dff7661c0f4e7428908c326ef8be2
faisalimroz <faisalimroz49@gmail.com>
2024-12-09 03:12:58 +0600

    [done] add skeleton component and update hero section

---
+++ src/constants/global.ts
@@ -0,0 +1,16 @@
+export const genderOptions = [
+    {
+        label: "Male",
+        value:"male"
+    },
+    {
+        label: "Female",
+        value:"female"
+    }
+]
+
+export  const modeOptions = [
+    { label: 'Basic', value: 'Basic' },
+    { label: 'Standard', value: 'Standard' },
+    { label: 'Premium', value: 'Premium' },
+  ];
